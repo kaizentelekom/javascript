d7aa24e5c713f3d56400a4bc7c6f90080356b976
AuroPick <aykutsakibusiness@gmail.com>
2023-06-03 00:57:13 +0300

    new configs

--- packages/eslint-config-kaizen-base/index.js
+++ packages/eslint-config-kaizen-base/index.js
@@ -1,4 +1,7 @@
 module.exports = {
+  env: {
+    es6: true
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   },
@@ -67,6 +70,92 @@ module.exports = {
     quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
 
     // use double quotes if jsx
-    'jsx-quotes': ['error', 'prefer-double']
+    'jsx-quotes': ['error', 'prefer-double'],
+
+    // show error if variable can be const
+    'prefer-const': 'error',
+
+    // show error if trying to reassign const
+    'no-const-assign': 'error',
+
+    // use let
+    'no-var': 'error',
+
+    // do not use object constructor
+    'no-new-object': 'error',
+
+    // use short version of object properties
+    'object-shorthand': 'error',
+
+    // do not use quotes if not need
+    'quote-props': ['error', 'as-needed'],
+
+    // do not use array constructor
+    'no-array-constructor': 'error',
+
+    // muust return in array functions
+    'array-callback-return': 'error',
+
+    // prefer destructuring
+    'prefer-destructuring': 'error',
+
+    // do not use spacing in literal template strings
+    'template-curly-spacing': 'error',
+
+    // do not use eval
+    'no-eval': 'error',
+
+    // show error for unnecessary escape
+    'no-useless-escape': 'error',
+
+    // do not define func inside loops
+    'no-loop-func': 'error',
+
+    // do not use arguments
+    'prefer-rest-params': 'error',
+
+    // do not use func constructor
+    'no-new-func': 'error',
+
+    'space-before-function-paren': [
+      'error',
+      { anonymous: 'always', named: 'never', asyncArrow: 'always' }
+    ],
+
+    // put space before blocks
+    'space-before-blocks': 'error',
+
+    // prefer spread
+    'prefer-spread': 'error',
+
+    // use arraw functions
+    'prefer-arrow-callback': 'error',
+
+    // put space before and after arrow
+    'arrow-spacing': 'error',
+
+    // always put arrow parentheses
+    'arrow-parens': 'error',
+
+    // do not use curly if not need
+    'arrow-body-style': 'error',
+
+    // put parantheses
+    'no-confusing-arrow': 'error',
+
+    // require the use of === and !==
+    eqeqeq: ['error', 'always', { null: 'ignore' }],
+
+    // disallow the use of Boolean literals in conditional expressions
+    // also, prefer `a || b` over `a ? a : b`
+    'no-unneeded-ternary': ['error', { defaultAssignment: false }],
+
+    // disallow use of multiple spaces
+    'no-multi-spaces': [
+      'error',
+      {
+        ignoreEOLComments: false
+      }
+    ]
   }
 }
